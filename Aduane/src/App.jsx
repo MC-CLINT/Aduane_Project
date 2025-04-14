a9d46e476a8a891bb7046c7ef401f22c03731468
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-04-14 12:03:33 +0000

    Some assets added

--- Aduane/src/App.jsx
+++ Aduane/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar/navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='myapp'>
+
+    < Navbar />
+
+    </div>
   )
 }
 
